e3fa46e65367e07a5a8a6af1c946a273860de0e6
Anthoussae <45543733+Anthoussae@users.noreply.github.com>
2025-05-30 11:53:59 +0800

    various debugs

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -1356,7 +1356,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52222" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64718" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
